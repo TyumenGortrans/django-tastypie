f7186b5d0595c0040e8d426690cff8d04136c0a6
Andrey Zevakin <azevakin@gmail.com>
2012-09-28 14:09:35 +0600

    Revert "Corrected ``Paginator.get_limit`` behavior when ``limit=0``."
    This reverts commit f407a58d15f2290dd79c73df4d23354b2201a3da.

--- tastypie/paginator.py
+++ tastypie/paginator.py
@@ -57,25 +57,17 @@ class Paginator(object):
 
         Default is 20 per page.
         """
-        settings_limit = getattr(settings, 'API_LIMIT_PER_PAGE', 20)
+        limit = getattr(settings, 'API_LIMIT_PER_PAGE', 20)
 
         if 'limit' in self.request_data:
             limit = self.request_data['limit']
         elif self.limit is not None:
             limit = self.limit
-        else:
-            limit = settings_limit
 
         try:
             limit = int(limit)
         except ValueError:
             raise BadRequest("Invalid limit '%s' provided. Please provide a positive integer." % limit)
-
-        if limit == 0:
-            if self.limit:
-                limit = self.limit
-            else:
-                limit = settings_limit
 
         if limit < 0:
             raise BadRequest("Invalid limit '%s' provided. Please provide a positive integer >= 0." % limit)
@@ -115,8 +107,9 @@ class Paginator(object):
         """
         Slices the result set to the specified ``limit`` & ``offset``.
         """
+        # If it's zero, return everything.
         if limit == 0:
-            raise BadRequest("Invalid limit '%s' provided. Please provide a positive, non-zero, integer." % limit)
+            return self.objects[offset:]
 
         return self.objects[offset:offset + limit]
 
